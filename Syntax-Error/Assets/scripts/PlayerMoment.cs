79d47e5fab75e40c34cad13ffeeb00ef279c280e
cypher4802 <tanmay258789@gmail.com>
2023-10-28 16:33:31 +0530

    basic

--- Syntax-Error/Assets/scripts/PlayerMoment.cs
+++ Syntax-Error/Assets/scripts/PlayerMoment.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+
+    }
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, body.velocity.y);
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
     }
 }
